8e43b454c7091259d8cecd48d225c8dcc40b444e
w3schools-test <108649227+sujithravivekanandh@users.noreply.github.com>
2022-07-07 18:44:20 +0530

    files added

---
+++ BasicBoxView1/BasicBoxView1/BasicBoxView1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace BasicBoxView1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
+
